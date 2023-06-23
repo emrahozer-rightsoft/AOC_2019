a5c6ba3501e6b38856922f4889b523c1ab5423b7
Emrah Ozer <emrahozer@rightsoft.co.uk>
2023-06-23 09:52:56 +0100

    added test cases

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,12 @@
 use std::{collections::HashSet, fs};
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("file not found");
+    let result = run("input.txt");
+    println!("point: {:?} and current_distance: {} ", result.0, result.1);
+}
+
+fn run(path: &str) -> (Point, i64) {
+    let input = fs::read_to_string(path).expect("file not found");
     let raw_directions: Vec<&str> = input.split('\n').collect();
     let wire_1_directions: Vec<Direction> = raw_directions[0].split(',').map(parse).collect();
     let wire_2_directions: Vec<Direction> = raw_directions[1].split(',').map(parse).collect();
@@ -29,11 +34,9 @@ fn main() {
         }
     }
 
-    println!(
-        "point: {:?} and current_distance: {} ",
-        current_point, min_distance
-    );
+    (current_point, min_distance)
 }
+
 fn calculate_stops(directions: &Vec<Direction>, stops: &mut HashSet<Point>) {
     let mut pos = Point { x: 0, y: 0 };
     for d in directions {
@@ -80,4 +83,27 @@ enum Direction {
 struct Point {
     x: i64,
     y: i64,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::run;
+
+    #[test]
+    fn test_input1() {
+        let result = run("test_input.txt");
+        assert_eq!(result.1, 159);
+    }
+
+    #[test]
+    fn test_input2() {
+        let result = run("test_input2.txt");
+        assert_eq!(result.1, 6);
+    }
+
+    #[test]
+    fn test_input3() {
+        let result = run("test_input3.txt");
+        assert_eq!(result.1, 135);
+    }
 }
